bd387edd47b1ff983f935a9bdb0873cafe595533
Admin <Admin@DESKTOP-J5OBS0H>
2022-07-13 17:30:34 +0700

    update code

---
+++ src/User/nopcomerce/PageUIs/User_LoginPageUI.java
@@ -0,0 +1,9 @@
+package User.nopcomerce.PageUIs;
+
+public class User_LoginPageUI {
+	public static final String EMAIL_TEXTBOX ="//input[@id='Email']";
+	public static final String PASSWORD_TEXTBOX ="//input[@id='Password']";
+	public static final String BUTTON_LOGIN ="//button[contains(@class,'login-button')]";
+	public static final String EMAIL_TEXTBOX_ERROR_MESSAGE = "//span[@id ='Email-error']";
+	public static final String UNSUCSSES_FULL_ERROR_MESSAGE ="//div[contains(@class,'message-error')]";
+}
---
+++ src/nopcommerce_Users/Basefactory.java
@@ -0,0 +1,42 @@
+package nopcommerce_Users;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import BaseObjectFactory.HomePageObject01;
+import BaseObjectFactory.RegisterObject01;
+import BasePageFactory.BasePageFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Basefactory extends BasePageFactory{
+	private WebDriver driver;
+	private HomePageObject01 HomePageObject01 = new HomePageObject01(driver);
+    private	RegisterObject01 RegisterObject01 = new RegisterObject01(driver);
+    BasePageFactory BasePageFactory;
+	
+@BeforeClass
+public void BeforeClass() {
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+	BasePageFactory = BasePageFactory.getBasePageFactory();
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	driver.get("https://demo.nopcommerce.com/");
+	HomePageObject01 = new HomePageObject01(driver);
+	RegisterObject01 = new RegisterObject01(driver);
+
+}
+
+@Test
+public void TC_01_Register_Emty_Data() {
+	HomePageObject01.clickToRegisterLink();
+	RegisterObject01.clickToRegisterButton();
+	Assert.assertEquals(RegisterObject01.getErrorMessageAtFirstnameTextbox(),"First name is required.");
+	Assert.assertEquals(RegisterObject01.getErrorMessageAtLastnameTextbox(),"Last name is required.");
+	Assert.assertEquals(RegisterObject01.getErrorMessageAtEmailTextbox(),"Email is required.");
+	Assert.assertEquals(RegisterObject01.getErrorMessageAtPasswordTextbox(),"Password is required.");
+	Assert.assertEquals(RegisterObject01.getErrorMessageAtConfirmPasswordTextbox(),"Password is required.");
+}
+}
