6aacfbaf33330db792fca945a6e235172e575649
nhinguyen95 <nhihien400@gmail.com>
2022-07-16 23:34:16 +0700

    update code

--- src/PageUI/nopcomerce/MyAccountUIs.java
+++
@@ -1,5 +0,0 @@
-package PageUI.nopcomerce;
-
-public class MyAccountUIs {
-	public static final String MY_ACCOUNT_LINK ="//a[contains(@class,'ico-account')]";
-}
--- src/nopcommerce_TCs/Framework_12_Switch_Page_UI.java
+++
@@ -1,5 +0,0 @@
-package nopcommerce_TCs;
-
-public class Framework_12_Switch_Page_UI {
-
-}
